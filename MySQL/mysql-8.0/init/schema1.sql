1cc37656b4fd9f1e17631ddf4af85abe579ed1fc
JulianArroyo <julian_arroyo79@hotmail.com>
2023-05-18 23:52:10 +0200

    main - Adminer ddbb client added - Init volume to create more than one schema added - SQL script to create schemas

---
+++ MySQL/mysql-8.0/init/schema1.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `schema1` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON `schema1`.* TO 'appuser'@'%';
---
+++ MySQL/mysql-8.0/init/schema2.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `schema2` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON `schema2`.* TO 'appuser'@'%';
